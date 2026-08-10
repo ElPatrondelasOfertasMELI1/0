43061d7a6d4d991ad02ad4c8428c590d7c77a021
ElPatrondelasOfertasMELI1 <conejotoystoys@gmail.com>
2026-08-10 14:21:30 -0600

    Actualizar carrusel.js

--- carrusel.js
+++ carrusel.js
@@ -136,6 +136,7 @@ document.addEventListener("DOMContentLoaded", () => {
             iniciar();
         }
     );
+    
     // =================================================
     // REINICIAR CUANDO CAMBIA EL TAMAÑO
     // =================================================
